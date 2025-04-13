a4c4c585afd693baacc1578f7c4f4138b53a6c6e
zarouz <zarouz1234@gmail.com>
2025-04-13 21:06:37 +0530

    Update core files and enhance .gitignore

--- users.session.sql
+++
@@ -1,5 +0,0 @@
--- Add the confirmation_sent_at column to the users table
-ALTER TABLE users
-ADD COLUMN confirmation_sent_at TIMESTAMP WITH TIME ZONE;
--- Optional: You can verify the change with:
--- \d users
